f5671079c78df1c26c3805eda6dabbcf5b9fd7b6
Mario Campos <mario.andres.campos@gmail.com>
2021-04-09 17:03:23 -0500

    Remove FILE_API #ifdef
    I don't much care for #ifdef if I can avoid it. It clutters the code and makes it hard to read. Only use it when absolutely necessary. And this is not absolutely necessary.

--- file.c
+++ file.c
@@ -1,9 +1,4 @@
-#ifdef FILE_API
-    #include <stdio.h>
-#else
-    #include <fcntl.h>
-#endif
-
+#include <stdio.h>
 #include "file.h"
 
 #define FILE_READ_BUFFER_SIZE BUFSIZ
@@ -11,7 +6,6 @@
 file_io_t *Open(const char *pathname, char *mode) {
     file_io_t *fd = Malloc(sizeof(file_io_t));
 
-#ifdef FILE_API
     fd->fd = NULL;
     if (str_equals(mode, "w")) {
         fopen_s(&fd->fd, pathname, "wb");
@@ -31,33 +25,10 @@ file_io_t *Open(const char *pathname, char *mode) {
     fd->mode = mode;
 
     return fd;
-#else
-    fd->fd = -1;
-
-    if (str_equals(mode, "w")) {
-        fd->fd = open(pathname, O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR);
-    } else if (str_equals(mode, "a")) {
-        fd->fd = open(pathname, O_WRONLY | O_APPEND, 0 /*ignored*/);
-    } else if (str_equals(mode, "r")) {
-        fd->fd = open(pathname, O_RDONLY, 0 /*ignored*/);
-    }
-
-    if (fd->fd == -1) {
-        logprintf(LOG_ERR, "Cant open the file %s. Exiting now.", pathname);
-        free(fd);
-        exitPgm(EXIT_FAILURE);
-    }
-
-    fd->fileName = pathname;
-    fd->mode = mode;
-
-    return fd;
-#endif
 }
 
 void Close(file_io_t *fd) {
     if (fd == NULL) return;
-#ifdef FILE_API
     if (fd->fd == NULL) return;
     int ret = fclose(fd->fd);
     if (ret != 0) {
@@ -65,21 +36,11 @@ void Close(file_io_t *fd) {
                   fd->fileName);
         exitPgm(EXIT_FAILURE);
     }
-#else
-    if (fd->fd == -1) return;
-    int ret = close(fd->fd);
-    if (ret == -1) {
-        logprintf(LOG_ERR, "Cant close the file %s. Exiting now.",
-                  fd->fileName);
-        exitPgm(EXIT_FAILURE);
-    }
-#endif
 
     free(fd);
 }
 
 void Seek(file_io_t *fd, long offset, int whence) {
-#ifdef FILE_API
     int ret;
     ret = fseek(fd->fd, offset, whence);
     if (ret == -1) {
@@ -87,16 +48,6 @@ void Seek(file_io_t *fd, long offset, int whence) {
                   fd->fileName);
         exitPgm(EXIT_FAILURE);
     }
-#else
-    off_t ret;
-    ret = lseek(fd->fd, offset, whence);
-    if (ret == (off_t)-1) {
-        perror("lseek");
-        logprintf(LOG_ERR, "Cant lseek the file %s. Exiting now.",
-                  fd->fileName);
-        exitPgm(EXIT_FAILURE);
-    }
-#endif
 }
 
 void readFile(char *filename, FileReader callback, void *ctx) {
--- file.h
+++ file.h
@@ -10,11 +10,7 @@
 #include "helper.h"
 
 struct file_io_t {
-#ifdef FILE_API
     FILE *fd;
-#else
-    int fd;
-#endif
     const char *fileName;
     char *mode;
 };    
@@ -22,56 +18,17 @@ struct file_io_t {
 typedef struct file_io_t file_io_t;
 typedef void (*FileReader) (void *buffer, unsigned int bytesRead, void *ctx);
 
-#ifdef FILE_API
 static inline size_t Read (file_io_t *fd, void *buf, size_t count) {
-#else
-static inline ssize_t Read (file_io_t *fd, void *buf, ssize_t count) {
-#endif
-#ifdef FILE_API
     size_t readBytes = fread(buf, 1, count, fd->fd);
     return readBytes;
-#else
-    ssize_t readBytes = 0;
-
-    do {
-        ssize_t ret = read(fd->fd, buf + readBytes, count-readBytes);
-        if (unlikely(ret == -1)) {
-            logprintf(LOG_ERR, "Cant read the file %s. Exiting now.", fd->fileName);
-            exitPgm(EXIT_FAILURE);
-        }
-        else if (ret == 0) {
-            return readBytes;
-        }
-
-        readBytes += ret;
-    } while (readBytes < count);
-
-    return readBytes;
-#endif
 }
 
-#ifdef FILE_API
 static inline void Write(file_io_t *fd, const void *buf, size_t count) {
-#else
-  static inline void Write(file_io_t *fd, const void *buf, ssize_t count) {
-#endif
-#ifdef FILE_API
     size_t written = fwrite(buf, 1, count, fd->fd);
     if (unlikely(written != count)) {
         logprintf(LOG_ERR, "Cant write the file %s. Exiting now.", fd->fileName);
         exitPgm(EXIT_FAILURE);
     }
-#else
-    ssize_t written = 0;
-    do {
-        ssize_t ret = write(fd->fd, buf + written, count-written);
-        written += ret;
-        if (unlikely(ret == -1)) {
-            logprintf(LOG_ERR, "Cant write the file %s. Exiting now.", fd->fileName);
-            exitPgm(EXIT_FAILURE);
-        }
-    } while(written < count);
-#endif
 }
     
 file_io_t* Open(const char *pathname, char *mode);
