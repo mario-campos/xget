7539b2ccf3057d2cd4d0e6d55e0efa77eb027ce8
Mario Campos <mario-campos@github.com>
2021-12-22 21:45:33 -0600

    Inline initCallbacks()

--- xdccget.c
+++ xdccget.c
@@ -837,24 +837,6 @@ accept_flag:
 }
 
 /*
- * This function configures the libircclient event loop.
- */
-void initCallbacks(irc_callbacks_t *callbacks) {
-    memset (callbacks, 0, sizeof(*callbacks));
-
-    callbacks->event_connect = event_connect;
-    callbacks->event_join = event_join;
-    callbacks->event_dcc_send_req = recvFileRequest;
-    callbacks->event_ctcp_rep = dump_event;
-    callbacks->event_ctcp_action = dump_event;
-    callbacks->event_unknown = dump_event;
-    callbacks->event_privmsg = dump_event;
-    callbacks->event_notice = event_notice;
-    callbacks->event_umode = event_umode;
-    callbacks->event_mode = event_mode;
-}
-
-/*
  * `init_signal` is a wrapper for `sigaction`; i.e. it defines
  * a signal handler for the given signal number.
  */
@@ -990,7 +972,17 @@ int main (int argc, char **argv)
     init_signal(SIGALRM, output_handler);
 
     irc_callbacks_t callbacks;
-    initCallbacks(&callbacks);
+    memset(&callbacks, 0, sizeof(callbacks));
+    callbacks.event_connect = event_connect;
+    callbacks.event_join = event_join;
+    callbacks.event_dcc_send_req = recvFileRequest;
+    callbacks.event_ctcp_rep = dump_event;
+    callbacks.event_ctcp_action = dump_event;
+    callbacks.event_unknown = dump_event;
+    callbacks.event_privmsg = dump_event;
+    callbacks.event_notice = event_notice;
+    callbacks.event_umode = event_umode;
+    callbacks.event_mode = event_mode;
     cfg.session = irc_create_session (&callbacks);
 
     if (!cfg.session) {
