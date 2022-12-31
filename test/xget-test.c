61cdee7dff369b131f4f290f73753c8cf3d4e876
Mario Campos <mario-campos@github.com>
2022-12-30 18:14:26 -0600

    Invert control of xget-test.c.
    This way, there is one "main event loop" (although it's not technically an event loop yet) function, `irc_run`, and it calls the appropriate callbacks based on certain commands. This is how libircclient works.

--- test/xget-test.c
+++ test/xget-test.c
@@ -11,18 +11,42 @@
 
 #define IRC_MSG_MAX_SIZE 512
 
-typedef struct {
+// TODO: delete this global variable
+pid_t xget_pid;
+
+typedef struct irc_server irc_server_t;
+typedef struct irc_session irc_session_t;
+typedef void (*irc_callback_t)(irc_session_t *, const char *);
+
+struct irc_server {
     int socket_fd;
     struct addrinfo *ai;
-} irc_session_t;
+    irc_callback_t on_accept;
+    irc_callback_t on_cmd_nick;
+    irc_callback_t on_cmd_user;
+    irc_callback_t on_cmd_join;
+    irc_callback_t on_cmd_quit;
+    irc_callback_t on_cmd_privmsg;
+};
 
-void irc_free(irc_session_t *session)
+struct irc_session {
+    int socket_fd;
+    irc_server_t *server;
+    char nick[32];
+    char user[32];
+    union {
+	struct sockaddr_storage sas;
+	struct sockaddr_in sai;
+    };
+};
+
+void irc_free(irc_server_t *server)
 {
-    freeaddrinfo(session->ai);
-    close(session->socket_fd);
+    freeaddrinfo(server->ai);
+    close(server->socket_fd);
 }
 
-void irc_serve(irc_session_t *session)
+void irc_serve(irc_server_t *server)
 {
     struct addrinfo hints = {
 	.ai_family = AF_INET,
@@ -30,38 +54,158 @@ void irc_serve(irc_session_t *session)
     };
 
     int errnum;
-    if ((errnum = getaddrinfo("127.0.0.1", "6667", &hints, &session->ai)))
+    if ((errnum = getaddrinfo("127.0.0.1", "6667", &hints, &server->ai)))
 	errx(EXIT_FAILURE, "getaddrinfo: %s", gai_strerror(errnum));
 
-    if ((session->socket_fd = socket(session->ai->ai_family, session->ai->ai_socktype, session->ai->ai_protocol)) == -1)
+    if ((server->socket_fd = socket(server->ai->ai_family, server->ai->ai_socktype, server->ai->ai_protocol)) == -1)
 	err(EXIT_FAILURE, "socket");
 
     // bind(2) may fail with "Address already in use." In that case, try to reuse the address if possible.
     int yes = 1;
-    setsockopt(session->socket_fd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof yes);
+    setsockopt(server->socket_fd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof yes);
 
-    if (bind(session->socket_fd, session->ai->ai_addr, session->ai->ai_addrlen))
+    if (bind(server->socket_fd, server->ai->ai_addr, server->ai->ai_addrlen))
 	err(EXIT_FAILURE, "bind");
 
-    if (listen(session->socket_fd, 100))
+    if (listen(server->socket_fd, 100))
 	err(EXIT_FAILURE, "listen");
 }
 
-int main(int argc, char *argv[])
+void irc_run(irc_server_t *server)
 {
-    int errnum;
-    irc_session_t session;
+    irc_session_t session = {.server = server};
 
+    socklen_t conn_sa_size = sizeof session.sas;
+    if ((session.socket_fd = accept(server->socket_fd, (struct sockaddr *) &session.sas, &conn_sa_size)) == -1)
+    {
+	kill(xget_pid, SIGINT);
+	wait(NULL);
+	err(EXIT_FAILURE, "accept");
+    }
+
+    server->on_accept(&session, NULL);
+
+    int counter;
+    char nick[IRC_MSG_MAX_SIZE], user[IRC_MSG_MAX_SIZE], channel[IRC_MSG_MAX_SIZE], peer[IRC_MSG_MAX_SIZE];
+    char buf[IRC_MSG_MAX_SIZE], param1[IRC_MSG_MAX_SIZE], param2[IRC_MSG_MAX_SIZE], param3[IRC_MSG_MAX_SIZE], param4[IRC_MSG_MAX_SIZE];
+    char *p = buf;
+
+    recv(session.socket_fd, buf, sizeof buf, 0);
+    if (!sscanf(p, "NICK %s\r\n%n", nick, &counter))
+    {
+	kill(xget_pid, SIGINT);
+	wait(NULL);
+	errx(EXIT_FAILURE, "expected IRC 'NICK' command");
+    }
+
+    server->on_cmd_nick(&session, nick);
+    p += counter;
+
+    if (!sscanf(p, "USER %s %s %s %s\r\n%n", user, param2, param3, param4, &counter))
+    {
+	kill(xget_pid, SIGINT);
+	wait(NULL);
+	errx(EXIT_FAILURE, "expected IRC 'USER' command");
+    }
+
+    server->on_cmd_user(&session, user);
+
+    recv(session.socket_fd, buf, sizeof buf, 0);
+    sscanf(buf, "JOIN %s\r\n%n", channel, &counter);
+
+    server->on_cmd_join(&session, channel);
+
+    int pack;
+    recv(session.socket_fd, buf, sizeof buf, 0);
+    sscanf(buf, "PRIVMSG %s :XDCC SEND #%d\r\n", peer, &pack);
+
+    server->on_cmd_privmsg(&session, peer);
+
+    recv(session.socket_fd, buf, sizeof buf, 0);
+    sscanf(buf, "QUIT %s\r\n", param1);
+
+    server->on_cmd_quit(&session, param1);
+}
+
+void cb_accept(irc_session_t *session, const char *null)
+{
+
+}
+
+void cb_cmd_nick(irc_session_t *session, const char *nick)
+{
+	strlcpy(session->nick, nick, sizeof session->nick);
+}
+
+void cb_cmd_user(irc_session_t *session, const char *user)
+{
+    char buf[IRC_MSG_MAX_SIZE];
+
+    // RPL_WELCOME
+    snprintf(buf, sizeof buf, ":127.0.0.1 001 %s :Welcome to libircserver %s!%s@127.0.0.1\r\n", session->nick, session->nick, user);
+    send(session->socket_fd, buf, strlen(buf), 0);
+
+    // RPL_YOURHOST
+    snprintf(buf, sizeof buf, ":127.0.0.1 002 %s :Your host is %s, running version %s\r\n", session->nick, "libircserver", "0.0.1");
+    send(session->socket_fd, buf, strlen(buf), 0);
+
+    // RPL_CREATED
+    snprintf(buf, sizeof buf, "127.0.0.1 003 %s :This server was created %s\r\n", session->nick, "16:43:38 Apr 23 2019");
+    send(session->socket_fd, buf, strlen(buf), 0);
+
+    // RPL_MYINFO
+    snprintf(buf, sizeof buf, "127.0.0.1 004 %s %s %s BGHIRSWcdhiorswx ABCDFGIJKLMNOPQRSTXYabcdefghijklmnopqrstuvwz FIJLXYabdefghjkloqvw\r\n", session->nick, "127.0.0.1", "0.0.1");
+    send(session->socket_fd, buf, strlen(buf), 0);
+
+    // RPL_ISUPPORT
+    snprintf(buf, sizeof buf, "127.0.0.1 005 %s MAXCHANNELS=30 :are supported by this server\r\n", session->nick);
+    send(session->socket_fd, buf, strlen(buf), 0);
+
+    strlcpy(session->user, user, sizeof session->user);
+}
+
+void cb_cmd_join(irc_session_t *session, const char *channel)
+{
+    char buf[IRC_MSG_MAX_SIZE];
+
+    // JOIN
+    snprintf(buf, sizeof buf, ":%s!%s@127.0.0.1 JOIN %s\r\n", session->nick, session->user, channel);
+    send(session->socket_fd, buf, strlen(buf), 0);
+
+    // RPL_TOPIC
+    snprintf(buf, sizeof buf, ":127.0.0.1 332 %s %s :this is the topic\r\n", session->nick, channel);
+    send(session->socket_fd, buf, strlen(buf), 0);
+
+    // RPL_NAMREPLY
+    snprintf(buf, sizeof buf, ":127.0.0.1 353 %s = %s :%s\r\n", session->nick, channel, session->nick);
+    send(session->socket_fd, buf, strlen(buf), 0);
+
+    // RPL_ENDOFNAMES
+    snprintf(buf, sizeof buf, ":127.0.0.1 366 %s %s :End of /NAMES list\r\n", session->nick, channel);
+    send(session->socket_fd, buf, strlen(buf), 0);
+}
+
+void cb_cmd_quit(irc_session_t *session, const char *message)
+{
+    char buf[IRC_MSG_MAX_SIZE];
+    snprintf(buf, sizeof buf, ":127.0.0.1 ERROR :Quit: ");
+    send(session->socket_fd, buf, strlen(buf), 0);
+    close(session->socket_fd);
+}
+
+void cb_cmd_privmsg(irc_session_t *session, const char *peer)
+{
+    char buf[IRC_MSG_MAX_SIZE];
     struct addrinfo *res2, hints = {
-	    .ai_family = AF_INET,
-	    .ai_socktype = SOCK_STREAM,
+	.ai_family = AF_INET,
+	.ai_socktype = SOCK_STREAM,
     };
-    irc_serve(&session);
-
-    struct sockaddr_in *saddr = (struct sockaddr_in *) session.ai->ai_addr;
 
     // Set up DCC listening socket
+    snprintf(buf, sizeof buf, ":%s PRIVMSG %s :\001DCC SEND %s %u %u %u\001\r\n", peer, session->nick, "file.txt", htonl(session->sai.sin_addr.s_addr), 6668, 1024);
+    send(session->socket_fd, buf, strlen(buf), 0);
 
+    int errnum;
     if ((errnum = getaddrinfo("127.0.0.1", "6668", &hints, &res2)))
 	errx(EXIT_FAILURE, "getaddrinfo: %s", gai_strerror(errnum));
 
@@ -78,6 +222,39 @@ int main(int argc, char *argv[])
     if (listen(dcc_sockfd, 100))
 	err(EXIT_FAILURE, "listen");
 
+    int xget_dcc_sockfd;
+    struct sockaddr_storage conn_sa;
+    socklen_t conn_sa_size = sizeof conn_sa;
+    if ((xget_dcc_sockfd = accept(dcc_sockfd, (struct sockaddr *) &conn_sa, &conn_sa_size)) == -1)
+    {
+	kill(xget_pid, SIGINT);
+	wait(NULL);
+	err(EXIT_FAILURE, "accept");
+    }
+
+    char file_buffer[1025] = {0};
+    memset(file_buffer, 'A', 1024);
+    send(xget_dcc_sockfd, file_buffer, strlen(file_buffer), 0);
+
+    unlink("file.txt");
+    close(xget_dcc_sockfd);
+    close(dcc_sockfd);
+    freeaddrinfo(res2);
+}
+
+int main(int argc, char *argv[])
+{
+    irc_server_t server = {
+	.on_accept = cb_accept,
+	.on_cmd_nick = cb_cmd_nick,
+	.on_cmd_user = cb_cmd_user,
+	.on_cmd_join = cb_cmd_join,
+	.on_cmd_privmsg = cb_cmd_privmsg,
+	.on_cmd_quit = cb_cmd_quit,
+    };
+
+    irc_serve(&server);
+
     // xget should be in the same directory as this executable, so modify argv[0] to form a path to xget.
     char *s = rindex(argv[0], 'x');
     strcpy(s, "xget");
@@ -93,7 +270,6 @@ int main(int argc, char *argv[])
 	    NULL,
     };
 
-    pid_t xget_pid;
     if ((xget_pid = fork()) == -1)
 	err(EXIT_FAILURE, "fork");
     if (0 == xget_pid) {
@@ -104,109 +280,8 @@ int main(int argc, char *argv[])
 	execvp(argv[0], xget_argv);
     }
 
-    int xget_sockfd;
-    struct sockaddr_storage conn_sa;
-    socklen_t conn_sa_size = sizeof conn_sa;
-    if ((xget_sockfd = accept(session.socket_fd, (struct sockaddr *) &conn_sa, &conn_sa_size)) == -1)
-    {
-	kill(xget_pid, SIGINT);
-	wait(NULL);
-	err(EXIT_FAILURE, "accept");
-    }
-
-    int counter;
-    char nick[IRC_MSG_MAX_SIZE], user[IRC_MSG_MAX_SIZE], channel[IRC_MSG_MAX_SIZE], peer[IRC_MSG_MAX_SIZE];
-    char buf[IRC_MSG_MAX_SIZE], param1[IRC_MSG_MAX_SIZE], param2[IRC_MSG_MAX_SIZE], param3[IRC_MSG_MAX_SIZE], param4[IRC_MSG_MAX_SIZE];
-    char *p = buf;
-
-    recv(xget_sockfd, buf, sizeof buf, 0);
-    if (!sscanf(p, "NICK %s\r\n%n", nick, &counter))
-    {
-	kill(xget_pid, SIGINT);
-	wait(NULL);
-	errx(EXIT_FAILURE, "expected IRC 'NICK' command");
-    }
-
-    p += counter;
-
-    if (!sscanf(p, "USER %s %s %s %s\r\n%n", user, param2, param3, param4, &counter))
-    {
-	kill(xget_pid, SIGINT);
-	wait(NULL);
-	errx(EXIT_FAILURE, "expected IRC 'USER' command");
-    }
-
-    // RPL_WELCOME
-    snprintf(buf, sizeof buf, ":127.0.0.1 001 %s :Welcome to libircserver %s!%s@127.0.0.1\r\n", nick, nick, user);
-    send(xget_sockfd, buf, strlen(buf), 0);
-
-    // RPL_YOURHOST
-    snprintf(buf, sizeof buf, ":127.0.0.1 002 %s :Your host is %s, running version %s\r\n", nick, "libircserver", "0.0.1");
-    send(xget_sockfd, buf, strlen(buf), 0);
-
-    // RPL_CREATED
-    snprintf(buf, sizeof buf, "127.0.0.1 003 %s :This server was created %s\r\n", nick, "16:43:38 Apr 23 2019");
-    send(xget_sockfd, buf, strlen(buf), 0);
-
-    // RPL_MYINFO
-    snprintf(buf, sizeof buf, "127.0.0.1 004 %s %s %s BGHIRSWcdhiorswx ABCDFGIJKLMNOPQRSTXYabcdefghijklmnopqrstuvwz FIJLXYabdefghjkloqvw\r\n", nick, "127.0.0.1", "0.0.1");
-    send(xget_sockfd, buf, strlen(buf), 0);
-
-    // RPL_ISUPPORT
-    snprintf(buf, sizeof buf, "127.0.0.1 005 %s MAXCHANNELS=30 :are supported by this server\r\n", nick);
-    send(xget_sockfd, buf, strlen(buf), 0);
-
-    recv(xget_sockfd, buf, sizeof buf, 0);
-    sscanf(buf, "JOIN %s\r\n%n", channel, &counter);
-
-    // JOIN
-    snprintf(buf, sizeof buf, ":%s!%s@127.0.0.1 JOIN %s\r\n", nick, user, channel);
-    send(xget_sockfd, buf, strlen(buf), 0);
-
-    // RPL_TOPIC
-    snprintf(buf, sizeof buf, ":127.0.0.1 332 %s %s :this is the topic\r\n", nick, channel);
-    send(xget_sockfd, buf, strlen(buf), 0);
-
-    // RPL_NAMREPLY
-    snprintf(buf, sizeof buf, ":127.0.0.1 353 %s = %s :%s\r\n", nick, channel, nick);
-    send(xget_sockfd, buf, strlen(buf), 0);
-
-    // RPL_ENDOFNAMES
-    snprintf(buf, sizeof buf, ":127.0.0.1 366 %s %s :End of /NAMES list\r\n", nick, channel);
-    send(xget_sockfd, buf, strlen(buf), 0);
-
-    int pack;
-    recv(xget_sockfd, buf, sizeof buf, 0);
-    sscanf(buf, "PRIVMSG %s :XDCC SEND #%d\r\n", peer, &pack);
-
-    snprintf(buf, sizeof buf, ":%s PRIVMSG %s :\001DCC SEND %s %u %u %u\001\r\n", peer, nick, "file.txt", htonl(saddr->sin_addr.s_addr), 6668, 1024);
-    send(xget_sockfd, buf, strlen(buf), 0);
-
-    int xget_dcc_sockfd;
-    if ((xget_dcc_sockfd = accept(dcc_sockfd, (struct sockaddr *) &conn_sa, &conn_sa_size)) == -1)
-    {
-	kill(xget_pid, SIGINT);
-	wait(NULL);
-	err(EXIT_FAILURE, "accept");
-    }
-
-    char file_buffer[1025] = {0};
-    memset(file_buffer, 'A', 1024);
-    send(xget_dcc_sockfd, file_buffer, strlen(file_buffer), 0);
-
-    recv(xget_sockfd, buf, sizeof buf, 0);
-    sscanf(buf, "QUIT %s\r\n", param1);
-
-    snprintf(buf, sizeof buf, ":127.0.0.1 ERROR :Quit: ");
-    send(xget_sockfd, buf, strlen(buf), 0);
-
-    unlink("file.txt");
-
-    freeaddrinfo(res2);
-    close(xget_dcc_sockfd);
-    close(dcc_sockfd);
-    close(xget_sockfd);
-    irc_free(&session);
+    irc_run(&server);
+    irc_free(&server);
 
     int xget_exit;
     wait(&xget_exit);
